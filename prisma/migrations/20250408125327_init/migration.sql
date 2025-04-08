231e718ea4e71b91d8a84124a9e1c31001262451
VerneZhong <verne.zhong@gmail.com>
2025-04-08 22:04:29 +0900

    テーブルの定義を修正

---
+++ prisma/migrations/20250408125327_init/migration.sql
@@ -0,0 +1,18 @@
+-- AlterTable
+ALTER TABLE `Post` ADD COLUMN `authorId` INTEGER NULL,
+    ADD COLUMN `published` BOOLEAN NOT NULL DEFAULT false,
+    MODIFY `content` TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
